b30b4fbf7f28a3005b68d454cf34d5b49f9dbaff
dneumair <46647186+dneumair@users.noreply.github.com>
2024-11-30 17:42:58 +0100

    switched to sfdisk

--- setup.sh
+++ setup.sh
@@ -5,26 +5,26 @@ echo "setting up raid1"
 diskA="/dev/nvme0n1"
 diskB="/dev/nvme2n1"
 
-setupDisk() {
+sfdisk /dev/nvme0n1 << EOF
+label: gpt
+label-id: $(uuidgen)
+device: /dev/sdX
+unit: sectors
 
-fdisk "${1:-"provide disk parameter"}" <<EOF
-g       # Create a new GPT partition table
-n       # Create a new partition (EFI)
-1       # Partition number 1
-        # Default - start at beginning of disk
-+512M   # Size of 512MB
-t       # Change partition type
-1       # Partition number 1
-uefi
-
-n       # Create a new partition (Linux)
-2       # Partition number 2
-        # Default - start immediately after previous partition
-        # Use the rest of the disk
-
-w       # Write changes to disk and exit
+# Partition table entries
+/dev/nvme0n1p1 : start=2048, size=1048576, type=U, name="EFI System Partition"
+/dev/nvme0n1p2 : start=1048576, type=83, name="Linux Filesystem"
 EOF
-}
+mkfs.fat -F32 /dev/sdX1 # EFI partition
 
-setupDisk $diskA
-setupDisk $diskB
+sfdisk /dev/nvme2n1 << EOF
+label: gpt
+label-id: $(uuidgen)
+device: /dev/sdX
+unit: sectors
+
+# Partition table entries
+/dev/nvme2n1p1 : start=2048, size=1048576, type=U, name="EFI System Partition"
+/dev/nvme2n1p2 : start=1048576, type=83, name="Linux Filesystem"
+EOF
+mkfs.fat -F32 /dev/sdX1 # EFI partition
