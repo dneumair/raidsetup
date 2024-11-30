032769244646d8a5ffd201f8ba9f84ff8f741955
dneumair <46647186+dneumair@users.noreply.github.com>
2024-11-30 18:10:20 +0100

    sfdisk

--- setup.sh
+++ setup.sh
@@ -5,26 +5,15 @@ echo "setting up raid1"
 diskA="/dev/nvme0n1"
 diskB="/dev/nvme2n1"
 
-sfdisk /dev/nvme0n1 << EOF
-label: gpt
-label-id: $(uuidgen)
-device: /dev/sdX
-unit: sectors
+sfdisk --wipe always --delete $diskA
+sfdisk --wipe always --delete $diskB
 
-# Partition table entries
-/dev/nvme0n1p1 : start=2048, size=1048576, type=U, name="EFI System Partition"
-/dev/nvme0n1p2 : start=1048576, type=83, name="Linux Filesystem"
+sfdisk $diskA << EOF
+size=512M, type=U
+size=+
 EOF
-mkfs.fat -F32 /dev/sdX1 # EFI partition
 
-sfdisk /dev/nvme2n1 << EOF
-label: gpt
-label-id: $(uuidgen)
-device: /dev/sdX
-unit: sectors
-
-# Partition table entries
-/dev/nvme2n1p1 : start=2048, size=1048576, type=U, name="EFI System Partition"
-/dev/nvme2n1p2 : start=1048576, type=83, name="Linux Filesystem"
+sfdisk $diskB << EOF
+size=512M, type=U
+size=+
 EOF
-mkfs.fat -F32 /dev/sdX1 # EFI partition
